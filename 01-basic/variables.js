e39123024488058d5a3472928321fb0a2bdbd685
Rishal Singh Dahiya <rishalsinghdahiya@Rishals-MacBook-Pro.local>
2025-03-14 18:49:11 +0530

    java script basic about variables

---
+++ 01-basic/variables.js
@@ -0,0 +1,13 @@
+let name  = "Rishal Singh Dahiya";
+let email = "rishal.dahiya@gmail.com";
+let gender = "male";
+let isMarried = true;
+let age = 33;
+
+console.log(`
+    my name is : ${name}
+    my age is : ${age}
+    my gender is : ${gender}
+    my email id is : ${email}
+    my marriage status is : ${isMarried}
+    `);
